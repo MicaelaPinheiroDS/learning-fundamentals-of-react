91610177d9071fcc70c3289233649abd43682953
Micaela Pinheiro <micaela.mews@gmail.com>
2023-02-10 21:06:56 -0300

    capturing variable and learning about state

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,19 @@
+import React, { useState } from 'react'
 import { Card } from '../../components/card'
 import './style.css'
 
 export function Home() {
+  const [studentName, setStudentName] = useState()
+
   const timeNow = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
 
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..."></input>
+      <h1>Nome..: {studentName}</h1>
+      <input type="text" 
+      placeholder="Digite o nome..."
+      onChange={e => setStudentName(e.target.value)}
+      ></input>
       <button>Adicionar</button>
       <Card name="Micaela" time={timeNow} />
     </div>
