5064d1c56e4a72b1ba87913be3b5ee726a331ffc
Micaela Pinheiro <micaela.mews@gmail.com>
2023-02-11 11:08:46 -0300

    updated new Date format(hour/minute/second) for use key card props

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,7 +12,8 @@ export function Home() {
         ['pt-BR'], 
         {
           hour: '2-digit', 
-          minute: '2-digit'
+          minute: '2-digit',
+          second: '2-digit'
         }
       )
     }
